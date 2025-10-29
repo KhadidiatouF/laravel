0aeb6e39b1f4dcd363615998712654aff812a094
Khadidiatou <jamiral2019@gmail.com>
2025-10-29 11:25:59 +0000

    fix bug

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,6 +10,15 @@ done
 echo "Database is up - executing migrations"
 php artisan migrate --force
 
+echo "Installing Passport keys"
+php artisan passport:install --force --no-interaction
+
+echo "Setting Passport keys in environment"
+PASSPORT_PRIVATE_KEY=$(cat storage/oauth-private.key | sed 's/$/\\n/' | tr -d '\n')
+PASSPORT_PUBLIC_KEY=$(cat storage/oauth-public.key | sed 's/$/\\n/' | tr -d '\n')
+echo "PASSPORT_PRIVATE_KEY=\"$PASSPORT_PRIVATE_KEY\"" >> .env
+echo "PASSPORT_PUBLIC_KEY=\"$PASSPORT_PUBLIC_KEY\"" >> .env
+
 echo "Seeding database"
 php artisan db:seed --force
 
