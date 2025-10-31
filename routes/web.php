1844f71a47d78bf2ad0bbcac69cf60dcf24940e2
Khadidiatou <jamiral2019@gmail.com>
2025-10-31 15:12:27 +0000

    fix bug

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 // */
 
 Route::get('/', function () {
-    return response()->json(['message' => 'API Laravel fonctionne', 'status' => 'OK', 'timestamp' => now()]);
+    return view('welcome');
 });
 
 use Illuminate\Support\Facades\Mail;
